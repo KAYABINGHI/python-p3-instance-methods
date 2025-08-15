1f1d93accf5b70e9d13f4238deaaf960775ae0bd
KAYABINGHI <muchaipauls@gmail.com>
2025-08-15 13:49:07 +0300

    dog&person

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,14 @@
 
 class Person:
     # Class body goes here
-
+  
     #Instance method definition
-    pass
+
+  def talk(self):
+        print('Hello World!')
+  def walk(self):
+        print('The person is walking.')
+
+fido = Person()
+fido.talk()
+fido. walk()
